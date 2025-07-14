5fe35b5350f5eefd53d8d09451a2fb63799c083b
Ifadha <198208527+Ifadha2004@users.noreply.github.com>
2025-07-14 09:38:07 +0530

    hooks issue up

--- src/components/ui/3d-card.jsx
+++ src/components/ui/3d-card.jsx
@@ -8,6 +8,7 @@ import React, {
   useContext,
   useRef,
   useEffect,
+  useCallback
 } from "react";
 
 const MouseEnterContext = createContext(undefined);
@@ -99,14 +100,23 @@ export const CardItem = ({
     handleAnimations();
   }, [isMouseEntered]);
 
-  const handleAnimations = () => {
+  // const handleAnimations = () => {
+  //   if (!ref.current) return;
+  //   if (isMouseEntered) {
+  //     ref.current.style.transform = `translateX(${translateX}px) translateY(${translateY}px) translateZ(${translateZ}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) rotateZ(${rotateZ}deg)`;
+  //   } else {
+  //     ref.current.style.transform = `translateX(0px) translateY(0px) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
+  //   }
+  // };
+
+  const handleAnimations = useCallback(() => {
     if (!ref.current) return;
     if (isMouseEntered) {
       ref.current.style.transform = `translateX(${translateX}px) translateY(${translateY}px) translateZ(${translateZ}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) rotateZ(${rotateZ}deg)`;
     } else {
       ref.current.style.transform = `translateX(0px) translateY(0px) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
     }
-  };
+  }, [isMouseEntered, translateX, translateY, translateZ, rotateX, rotateY, rotateZ]);
 
   return (
     <Tag
